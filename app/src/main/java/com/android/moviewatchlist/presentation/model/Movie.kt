eacd1c261aada30e52a62fdf5dd1e2ccc10cba02
Rudem <ernesoglu@gmail.com>
2021-10-31 20:05:19 +0200

    Add movie screen

---
+++ app/src/main/java/com/android/moviewatchlist/presentation/model/Movie.kt
@@ -0,0 +1,9 @@
+package com.android.moviewatchlist.presentation.model
+
+data class Movie(
+    val id: Int,
+    val name: String,
+    val year: String,
+    val description: String,
+    val imdbScore: Double
+)
---
+++ app/src/main/java/com/android/moviewatchlist/presentation/movie/MovieActivity.kt
@@ -0,0 +1,13 @@
+package com.android.moviewatchlist.presentation.movie
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.moviewatchlist.R
+
+class MovieActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie)
+    }
+}
